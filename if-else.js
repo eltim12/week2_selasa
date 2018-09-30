00127fa5e999644de6b761e2d5413566e0265bb6
eltim12 <eltim2000@gmail.com>
2018-09-30 23:39:53 +0700

    first commit

---
+++ if-else.js
@@ -0,0 +1,18 @@
+var nama= "Elijah";
+var peran = "Vampire";
+
+if (nama === "" && peran === ""){
+    console.log ("nama harus di isi!");
+} else if (nama === "Niklaus") {
+    console.log ("Halo " + nama + ", Pilih peranmu untuk memulai game!");
+} else if (nama === "Hayley" && peran === "Werewolf") {
+    console.log ("Selamat datang di New Orleans, " + nama)
+    console.log ("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!");
+} else if (nama === "Elijah" && peran === "Vampire") {
+    console.log ("Selamat datang di New Orleans, " + nama)
+    console.log ("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!");
+} else if (nama === "Freya" && peran === "Witch") {
+    console.log ("Selamat datang di New Orleans, " + nama)
+    console.log ("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!");
+}
+ 
